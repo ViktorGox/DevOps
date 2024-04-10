cfb25de3369c04c757c78253c41eec38b8dcdc60
Borislav Markov <533390@student.saxion.nl>
2024-04-10 13:26:14 +0300

    Added the creation of a new instance called database

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,10 @@
-# Your variables here
+variable "user_data_script" {
+  type    = string
+  default = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    EOF
+}
