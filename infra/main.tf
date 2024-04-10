53d145d1ef5992bfc964b2e5540f68e421841cd4
Borislav Markov <533390@student.saxion.nl>
2024-04-10 13:40:19 +0300

    Trying to run the database on an instance using CI/CD

--- infra/main.tf
+++ infra/main.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "instance_sg" {
   }
 
   ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
