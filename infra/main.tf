6dd63afceb2b8706623bb5e413fef88385207a65
VitkorKrGo <viktor.krastev03@gmail.com>
2024-04-12 18:05:27 +0200

    Adding some more stuff to the tf.

--- infra/main.tf
+++ infra/main.tf
@@ -72,18 +72,18 @@ resource "aws_vpc" "vpc" {
 #  }
 #}
 #
-#resource "aws_internet_gateway" "igw" {
-#  vpc_id = aws_vpc.vpc.id
-#}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
 #
-#resource "aws_route_table" "route_table" {
-#  vpc_id = aws_vpc.vpc.id
-#
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.igw.id
-#  }
-#}
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
 #
 #
 #resource "aws_route_table_association" "example" {
