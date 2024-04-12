0a6bbfdff99dc850b949a308f568e26b737a543f
VitkorKrGo <viktor.krastev03@gmail.com>
2024-04-12 17:54:57 +0200

    Removing vpc from tf.

--- infra/main.tf
+++ infra/main.tf
@@ -13,9 +13,9 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+#resource "aws_vpc" "vpc" {
+#  cidr_block = "10.0.0.0/16"
+#}
 
 #resource "aws_security_group" "instance_sg" {
 #  name        = "instance-sg"
